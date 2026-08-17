e6e4657cb8f76b5b7435f968b92ad32829f35648
dm-morozov <dem.morozov@gmail.com>
2026-08-17 11:51:43 +0300

    fiat(load_initial_stock): Спарсили csv файл магизина с товарами. Пока код не оптимизирован и потребляет много память. Нерациональное использование DictReader, datetime, и в целом вывод даты в одном дне является избыточным. Мы ее тоже уберем. Но промежуточный вариант сохраним.

--- preparing-for-the-interview/code_experiments/05_test_task_InfoVizion/stock.py
+++ preparing-for-the-interview/code_experiments/05_test_task_InfoVizion/stock.py
@@ -1,19 +1,34 @@
 from pathlib import Path
+import csv 
+import datetime
+from pprint import pp
+from itertools import islice
 
 PATH_SOURCE = Path(__file__).parent
 PATH_TRANS = PATH_SOURCE / 'invent_trans'
 PATH_STOCK = PATH_SOURCE / 'stock'
 
 def load_initial_stock(file_path: Path) -> dict:
-    pass
+    data = {}
+    with open(file_path, 'r', newline='', encoding='utf-8') as file_handle:
+        reader = csv.DictReader(file_handle, delimiter=';')
+
+        for row in islice(reader, 3):
+            data[(row['item_id'], row['location_id'])] = {
+                'date': datetime.datetime.strptime(row['trans_date'], '%Y-%m-%d').date(),
+                'quantity': float(row['qty']),
+                'price': float(row['cost_amount'])
+            }
+    return data
 
 
 def main() -> None:
-    pass
-
+    file_path = PATH_STOCK.joinpath('stock_2025_04_30.csv')
+    initial_stock = load_initial_stock(file_path)
+    pp(initial_stock)
 
 if __name__ == "__main__":
     try:
         main()
-    except Exception:
-        print('Oops... Something wrong!')
+    except Exception as e:
+        print(f'Oops... Something wrong: {e}')
